cf1b917c76dbe80cb572fcafbe121d12a7d4814d
Anutithi4309 <elorasumi1977@gmail.com>
2025-06-12 22:09:06 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+my_dict={}
+my_dict={1:'apple',2:'ball'}
+my_dict={'name':'anu',1:[2,4,3]}
+my_dict={'name':'sarvia','age':12}
+print(my_dict['name'])
+print(my_dict.get('age'))
+my_dict['age']=13
+print(my_dict)
+my_dict.pop('age')
+print(my_dict)
+print("Address:",my_dict.get('address'))
+my_dict.clear()
+print(my_dict)
